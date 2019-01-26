9518276b66fe092d18e863e4c2d65b7524a4b9ea
Andrei Thompson <andreiwthomps@gmail.com>
2019-01-26 13:10:43 -0800

    Uwuchi is born

---
+++ Assets/Scripts/UchiBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UchiBehavior : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
