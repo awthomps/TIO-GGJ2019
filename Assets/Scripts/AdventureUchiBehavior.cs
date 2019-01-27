54e615d2230f27ba8d8b5de353ba2eb8dc6759bd
Andrei Thompson <andreiwthomps@gmail.com>
2019-01-26 17:04:27 -0800

    Implement some basic but broken platforming

---
+++ Assets/Scripts/AdventureUchiBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventureUchiBehavior : MonoBehaviour
+{
+    public float speed;
+    public float jumpMagnitude;
+    private Rigidbody2D rb;
+    private int groundContact;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        groundContact = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float jumpValue = Input.GetAxis("Jump");
+        float moveVertical = 0.0f;
+        if (jumpValue > 0.0f && groundContact > 0)
+        {
+            moveVertical = jumpMagnitude;
+            Debug.Log(jumpMagnitude);
+            groundContact = 0;
+        }
+
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement);
+    }
+
+    void OnCollisionEnter2D(Collision2D collided)
+    {
+        if (collided.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log(collided.gameObject.tag);
+            groundContact++;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collided)
+    {
+        if (collided.gameObject.CompareTag("Ground") && groundContact > 0)
+        {
+            Debug.Log(collided.gameObject.tag);
+            groundContact++;
+        }
+    }
+}
