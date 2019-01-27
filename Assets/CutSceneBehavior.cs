2aa014d41fceef369595bfc0b14e66ca7bebed1b
Andrei Thompson <andreiwthomps@gmail.com>
2019-01-27 15:37:16 -0800

    Create Intro Cutscene

---
+++ Assets/CutSceneBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneBehavior : MonoBehaviour
+{
+    public int scrollForce = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(0, scrollForce));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/EndCreditDetectorBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCreditDetectorBehavior : MonoBehaviour
+{
+
+    public string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Reached end of CutScene");
+        if(collision.gameObject.CompareTag("EndCutScene"))
+        {
+            //Go back home!:
+            SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+        }
+    }
+}
